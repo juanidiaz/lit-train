77b616b573bea03a8f9f007a5d43e240ec795d18
Juan I Diaz <jdi@idiaz.ca>
2019-10-31 21:57:25 -0400

    Simple stuff

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Playing with lit-HTML
 
 Following info from this site:
-https://dev.to/julcasans/lit-html-templates-from-zero-to-hero-for-beginners-3eje
+https://dev.to/julcasans/a-gentle-introduction-to-lit-html-3d74
+
+## Before you do anything You need to install ``lit-HTML``
+
+So, run this command: ``npm install lit-html``
+
---
+++ password-checker.js
@@ -0,0 +1,41 @@
+import { html, render } from 'lit-html';
+
+class PasswordChecker extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.password = this.getAttribute('password');
+    this.clave = this.getAttribute('password');
+    this.tryID = this.getAttribute('tryID');
+    console.log("Password", this.password);
+    console.log("Clave", this.clave);
+    console.log("TryID", this.tryID);
+  }
+
+  get password() { return this._password; }
+
+  set password(value) {
+    this._password = value;
+    this.setAttribute('password', value);
+    this.update();
+  }
+
+  update() {
+    render(this.template(), this.shadowRoot, {eventContext: this});
+  }
+
+  isValid(passwd) { 
+    const re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}/;
+    return re.test(passwd);
+  }
+
+  template() {
+    return html`
+      <span>Your password is <strong>${this.isValid(this.password) ? 'valid 👍' : 'INVALID 👎'}</strong></span>
+      ${this.isValid(this.password) ? 
+        html`<div>Strength: <progress value=${this.password.length-3} max="5"</progress></div>` : ``}`;
+
+  }
+}
+
+customElements.define('password-checker', PasswordChecker);
